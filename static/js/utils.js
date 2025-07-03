d8d921f1df1ea2147c7934b12f740981e64fe3ee
Stephen Zweibel <oneperstephen@gmail.com>
2025-07-03 14:53:20 -0400

    Update interlibrary loan description for clarity and add fallback class to events section

--- static/js/utils.js
+++ static/js/utils.js
@@ -192,7 +192,7 @@ function handleEventsSuccess(result) {
 
     if (allEvents.length === 0) {
         console.log("No events found");
-        $("#events").html('<h3 class="lower-header">Explore Our Digital Projects</h3><iframe title="Content Box frame" id="s-lg-widget-frame-1716564549198" width="" height="" scrolling="no" style="height: 410px; width: 90%;" src="//lgapi-us.libapps.com/widget_box.php?site_id=146&widget_type=8&output_format=2&widget_title=Digital+Collections+Gallery&widget_height=&widget_width=&widget_embed_type=1&guide_id=1204677&box_id=32696065&map_id=38442276&content_only=0&include_jquery=0&config_id=1716564549198"></iframe>');
+        $("#events").html('<h3 class="lower-header">Explore Our Digital Projects</h3><iframe title="Content Box frame" id="s-lg-widget-frame-1716564549198" width="" height="" scrolling="no" style="height: 410px; width: 90%;" src="//lgapi-us.libapps.com/widget_box.php?site_id=146&widget_type=8&output_format=2&widget_title=Digital+Collections+Gallery&widget_height=&widget_width=&widget_embed_type=1&guide_id=1204677&box_id=32696065&map_id=38442276&content_only=0&include_jquery=0&config_id=1716564549198"></iframe>').addClass('digital-projects-fallback');
 
         $('.events-section').removeClass('col-md-8').addClass('col-md-7');
     } else {
